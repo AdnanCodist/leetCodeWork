8181f4b6c2755c7cc26a42d50ddbe5dc47af729c
AdnanCodist <adnanjahangir54@gmail.com>
2026-09-23 20:53:27 +0530

    Time: 4 ms (98.07%), Space: 102.2 MB (29.35%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,46 +1,42 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
         int n = nums.length;
+        int totalsum = 0;
 
+        for (int num : nums) {
+            totalsum += num;
+        }
+
+        int remain = totalsum - x;
+
+        // edge case
+        if (remain < 0)
+            return -1;
+
+        int left = 0;
         int sum = 0;
         int max = -1;
 
-        // try both
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // left [] right
 
-        map.put(0, -1);
+        // increase to right
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
 
-        int totalSum = 0;
+            // shrink from left if sum is bigger than required
+            while (sum > remain) {
+                sum -= nums[left];
+                left++;
+            }
 
-        for (int num : nums) {
-            totalSum += num;
-        }
-
-        int remain = totalSum - x;
-
-        for (int i = 0; i < n; i++) {
-
-            sum += nums[i];
-            map.put(sum, i);
-
-            if (map.containsKey(sum - remain)) {
-                int start = map.get(sum - remain);
-
-                int len = i - start;
-
+            //found a subarray where sum is total - x
+            if (sum == remain) {
+                int len = right - left + 1;
                 max = Math.max(max, len);
             }
-
-            if (!map.containsKey(sum)) {
-                map.put(sum, i);
-            }
         }
 
-        if (max == -1) {
-            return -1;
-        }
-
-        return n - max;
+        return max == -1 ? -1 : n - max;
         // return
     }
 }
